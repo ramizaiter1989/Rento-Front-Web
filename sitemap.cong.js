fc7f19b032439885c82189ab391a0f107a8f3f0b
ramizeaiter <raamizaiter1989@gmail.com>
2025-12-08 23:49:34 +0100

    bing SEO 1 sitemap

---
+++ sitemap.cong.js
@@ -0,0 +1,12 @@
+export default {
+  hostname: "https://rento-lb.com",   // your real domain
+  routes: [
+    "/",
+    "/intro",  
+    "/about",
+    "/cars",
+    "/contact",
+    "/login",
+    "/register"
+  ]
+};
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import sitemap from 'vite-plugin-sitemap';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: "https://rento-lb.com", // <-- replace with your domain
+    })
+  ],
   server: { 
     port: 4000,
-    host: "127.0.0.1",      // force IPv4 instead of ::1
-    strictPort: true        // prevents random port change
+    host: "127.0.0.1",
+    strictPort: true
   },
   resolve: {
     alias: { "@": path.resolve(__dirname, "src") },
